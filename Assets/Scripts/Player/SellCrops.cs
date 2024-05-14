9ecf277cb169f9a499ab91532dcc7cdabd787ae2
Andrews <jp14774@ms.wasatch.edu>
2024-05-14 13:19:06 -0600

    stuffffff

--- Assets/Scripts/Player/SellCrops.cs
+++ Assets/Scripts/Player/SellCrops.cs
@@ -1,11 +1,16 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using SaveLoad;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class SellCrops : MonoBehaviour
 {
-    [SerializeField] public int stockPrice = 100;
+    [SerializeField] TextMeshProUGUI textBoxPrice;
+    [SerializeField] public int stockPrice;
     [SerializeField] public float RandyTime;
     
     void Update()
@@ -18,8 +23,11 @@ public class SellCrops : MonoBehaviour
         {
             RandyTime = Random.Range(60, 600); //1-10 minute random
             stockPrice = Random.Range(50, 200);
+            textBoxPrice.text = stockPrice.ToString("C0");
         }
+        
     }
+
 
     public void SellCropsForMoney(float cropsToSell)
     {
